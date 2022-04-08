bc5667116da4dbfd507fd67fb5e574782ad1c043
wiamRB <101800097+wiamRB@users.noreply.github.com>
2022-04-08 20:22:33 +0000

    Update UDPserver.py

--- UDPserver.py
+++ UDPserver.py
@@ -2,10 +2,10 @@
 from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind((”, serverPort))
-print ‘The Server is ready to receive requests from Clients’
+serverSocket.bind("", serverPort))
+print ("The Server is ready to receive requests from Clients")
 while True:
 messagefromclient, clientAddress = serverSocket.recvfrom(2048)
-print ‘Message from Client: ‘, messagefromclient
+print ("Message from Client: ", messagefromclient)
 messagetoclient = raw_input(‘Enter Reply Message to Client: ‘)
 serverSocket.sendto(messagetoclient, clientAddress)
